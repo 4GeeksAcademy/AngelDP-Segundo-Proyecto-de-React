947b93e03a46f7e602845d17ff4c2395f4973e33
Angel Ponte <angeld0606@gmail.com>
2025-02-06 13:25:52 +0000

    Ejercicio resuelto con un botón de pausa

--- src/js/pages/home.jsx
+++ src/js/pages/home.jsx
@@ -5,38 +5,51 @@ import './home.css';
 const Home = () => {
 
 	const [counterValues, setCounterValues] = useState([0, 0, 0, 0, 0, 0]);
+	const [counting, setCounting] = useState(true);
 
 	useEffect(() => {
-		const interval = setInterval(() => {
-			setCounterValues(prevValue => {
-				const newValues = [...prevValue];
-				newValues[5] = (newValues[5] + 1) % 10;
 
-				if (newValues[5] === 0) {
-					newValues[4]++;
-				}
+		let interval = null;
 
-				if (newValues[4] === 10){
-					newValues[3]++
-					newValues[4] = 0
-				}
-
-				if (newValues[3] === 10){
-					newValues[2]++
-					newValues[3] = 0
-				}
-
-				if (newValues[2] === 10){
-					newValues[1]++
-					newValues[2] = 0
-				}
-
-				return newValues;
-			});
-		}, 1000);
-
+		if(counting){
+			 interval = setInterval(() => {
+				setCounterValues(prevValue => {
+					const newValues = [...prevValue];
+					newValues[5] = (newValues[5] + 1) % 10;
+	
+					if (newValues[5] === 0) {
+						newValues[4]++;
+					}
+	
+					if (newValues[4] === 10){
+						newValues[3]++
+						newValues[4] = 0
+					}
+	
+					if (newValues[3] === 10){
+						newValues[2]++
+						newValues[3] = 0
+					}
+	
+					if (newValues[2] === 10){
+						newValues[1]++
+						newValues[2] = 0
+					}
+	
+					return newValues;
+				});
+			}, 1000);
+		}else {
+			clearInterval(interval);
+		}
+		
 		return () => clearInterval(interval);
-	}, []);
+
+	}, [counting]);
+
+	const stopPlay = () => {
+		setCounting(!counting);
+	};
 
 	return (
 		<>
@@ -47,6 +60,9 @@ const Home = () => {
 						<SecondCounter key={index} item={value} />
 					))}
 				</div>
+				<button type="button" className="btn btn-light m-3" onClick={stopPlay}>
+					<i className="fa-solid fa-pause"></i>  <i className="fa-solid fa-play"></i>
+				</button>
 			</div>
 		</>
 	);
